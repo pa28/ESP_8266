455e9f0adaf3a3a6dbad9dffe726d82b84a30628
Richard Buckley <richard@hrbuckley.net>
2018-01-22 17:16:51 -0500

    Refactor the code to use the Adafruit unified sensor library and operate on an Adafruit Hazzah ESP 8266.

--- HomeData/HomeData.ino
+++ HomeData/HomeData.ino
@@ -1,3 +1,5 @@
+
+
 /*
  * A sketch for an ESP8266 NodeMCU 1.0 module for home data collection.
  * 
@@ -27,29 +29,22 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
 #include <WiFiClientSecure.h>
-#include <SimpleDHT.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+
 
 #define SERIAL 0          // Set to 1 for serial logging
 #define NO_SLEEP 0        // Set to 1 to use delay instead of deep sleep
 #define SECURE 0          // Set to use TLS
-
-const int LED_PIN = 5;    // Define the activity LED pin
-const int DHT11_PIN = 4;  // Define the DHT11 pin
+#define LED_ON LOW
+#define LED_OFF HIGH
 
 /*
- * Macros to implement simple serial logging if SERIAL is 1
+ * Configuration for Adafruit Hazza 8266
  */
-#if SERIAL
-#define PRINT(a) Serial.print((a))
-#else
-#define PRINT(a)
-#endif
-
-#if SERIAL
-#define PRINTln(a) Serial.println((a))
-#else
-#define PRINTln(a)
-#endif
+const int LED_PIN = 2;    // Define the activity LED pin, blue
+const int DHT11_PIN = 14; // Define the DHT11 pin
 
 /*
  * Manage multiple WiFi connections using ESP8266WiFiMulti
@@ -87,20 +82,23 @@ const int   httpsPort = 443;
 /*
  * Sleep and delay time settings.
  */
-const int   successSleep = 120e6;         // Time to sleep if reading successfully sent to server (uS)
+const int   successSleep = 600e6;         // Time to sleep if reading successfully sent to server (uS)
 const int   failSleep = 60e6;             // Time to sleep if reading unsucessfull (uS)
 const int   successDelay = 60e3;          // Delay if reading successfully sent to server (mS)
 const int   failDelay = 5e3;              // Delay if reading unsuccessfull (mS)
 const int   notConnectedDelay = 10e3;     // Delay if not connected to hot spot (mS)
 const int   serialSpeed = 115200;         // Serial baud rate if used
 
-byte temperature = 0;                     // Storage for temperature reading
-byte humidity = 0;                        // Storage for humidity reading
-
+float temperature = 0;                    // Storage for temperature reading
+float humidity = 0;                       // Storage for humidity reading
+uint32_t delayMS;
+bool tempOk = false;
+bool humiOk = false;
 /*
- * DHT11 object using the Simple DHT library
+ * DHT11 unified library object
  */
-SimpleDHT11 DHT;                          // DHT11 object
+
+DHT_Unified dhtObj(DHT11_PIN, DHT11);
 
 /*
  * Setup
@@ -119,19 +117,52 @@ void setup() {
    * Setup the LED pin. Turn on the LED to indicate the sketch is active
    */
   pinMode(LED_PIN, OUTPUT);
-  digitalWrite(LED_PIN, HIGH);
+  digitalWrite(LED_PIN, LED_ON);
 
   /*
    * Display the device hostname for debugging
    */
-  PRINT("I am ");
-  PRINTln(wifi.hostname());
-  
+#if SERIAL
+  Serial.print("I am ");
+  Serial.println(wifi.hostname());
+#endif
+
   /*
    * Add available hot spot credentials here
    */
   wifiMulti.addAP(ssid1, password1);
   wifiMulti.addAP(ssid2, password2);
+
+  /*
+   * Pre read DHT11
+   */
+  dhtObj.begin();
+  sensor_t sensor;
+  dhtObj.temperature().getSensor(&sensor);
+#if SERIAL
+  Serial.println("------------------------------------");
+  Serial.println("Temperature");
+  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
+  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
+  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
+  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" *C");
+  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" *C");
+  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" *C");  
+  Serial.println("------------------------------------");
+#endif
+  dhtObj.humidity().getSensor(&sensor);
+#if SERIAL
+  Serial.println("------------------------------------");
+  Serial.println("Humidity");
+  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
+  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
+  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
+  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println("%");
+  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println("%");
+  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println("%");  
+  Serial.println("------------------------------------");
+#endif
+  delayMS = sensor.min_delay / 1000;
 }
 
 /*
@@ -142,8 +173,6 @@ void delayOrSleep( int delayMilliSeconds, int sleepMicroSeconds )
 #if NO_SLEEP
   delay( delayMilliSeconds );
 #else
-  PRINT("Sleeping ");
-  PRINTln( sleepMicroSeconds );
   delay(100);
   ESP.deepSleep( sleepMicroSeconds );
 #endif
@@ -158,17 +187,25 @@ void monitorWiFi()
   if (wifiMulti.run() != WL_CONNECTED) {
     if (connectionWasAlive == true) {
       connectionWasAlive = false;
-      PRINT("Loooking for WiFi");
+#if SERIAL
+      Serial.print("Loooking for WiFi");
+#endif
     }
-    PRINT(".");
+#if SERIAL
+    Serial.print(".");
+#endif
+    digitalWrite(LED_PIN, LED_OFF);
     delay(500);
+    digitalWrite(LED_PIN, LED_ON);
   } else {
+#if SERIAL
     if (connectionWasAlive == false) {
-      PRINT(" connected to ");
-      PRINTln(WiFi.SSID().c_str());
-      PRINT("Wi-Fi mode set to WIFI_STA ");
-      PRINTln(WiFi.mode(WIFI_STA) ? "" : "Failed!");
+      Serial.print(" connected to ");
+      Serial.println(WiFi.SSID().c_str());
+      Serial.print("Wi-Fi mode set to WIFI_STA ");
+      Serial.println(WiFi.mode(WIFI_STA) ? "" : "Failed!");
     }
+#endif
     connectionWasAlive = true;
   }
 }
@@ -193,18 +230,29 @@ void loop()
      * Read the DHT11
      * 
      */
-    int chk = SimpleDHTErrSuccess;
-    chk = DHT.read(DHT11_PIN, &temperature, &humidity, NULL);
-    if (chk != SimpleDHTErrSuccess) {
-      PRINT("Read DHT11 failed, err=");
-      PRINTln(chk);
-      delay(100);
-    } else {
-      PRINT("Temperature = ");
-      PRINTln((int)temperature);
-      PRINT("Humidity =    ");
-      PRINTln((int)humidity);
+    delay(delayMS);
+    sensors_event_t event;
+    dhtObj.temperature().getEvent(&event);
+    if (!isnan(event.temperature)) {
+      tempOk = true;
+      temperature = event.temperature;
     }
+    dhtObj.humidity().getEvent(&event);
+    if (!isnan(event.relative_humidity)) {
+      humiOk = true;
+      humidity = event.relative_humidity;
+    }
+#if SERIAL
+    if (humiOk) {
+      Serial.print("Humidity ");
+      Serial.println(humidity,1);
+    }
+    if (tempOk) {
+      Serial.print("Temperature ");
+      Serial.println(temperature,1);
+    }
+#endif
+    
 
 #if SECURE
     WiFiClientSecure client;
@@ -212,9 +260,11 @@ void loop()
     WiFiClient client;
 #endif
 
-    PRINT("\n[Connecting to ");
-    PRINT(host);
-    PRINT(" ... ");
+#if SERIAL
+    Serial.print("\n[Connecting to ");
+    Serial.print(host);
+    Serial.print(" ... ");
+#endif 
 
     /*
      * Connect to the reporting host and send the reading.
@@ -226,44 +276,70 @@ void loop()
     if (client.connect(host, httpPort))
 #endif
     {
-      PRINTln("connected]");
+      digitalWrite(LED_PIN, LED_OFF);
 
-      PRINTln(String("[Sending a request] ") +
-                   url + "?s=" + wifi.hostname() + "&t=" + temperature + "&h=" + humidity + "&e=" + chk
-                    );
+      String tempData = "environment,sensor=" + wifi.hostname() + ",room=test temperature=" + temperature + '\n';
+      String humiData = "environment,sensor=" + wifi.hostname() + ",room=test humidity=" + humidity + '\n';
 
-      client.print(String("GET ") +
-                   url + "?s=" + wifi.hostname() + "&t=" + temperature + "&h=" + humidity + "&e=" + chk +
-                   " HTTP/1.1\r\n" +
+#if SERIAL
+      Serial.println("connected]");
+
+      Serial.println(String("[Sending a request] POST") + url +  " HTTP/1.1\n" +
+                   "Host: " + host + "\n" +
+                   "Connection: close\n" +
+                   "Content-Length: " + (tempData.length()+humiData.length()) + "\n" +
+                   "Content-Type: text/plain\n" +
+                   "\n"
+                  );
+      Serial.print(tempData);
+      Serial.print(humiData);
+#endif
+      client.print(String("POST ") + url + " HTTP/1.1\r\n" +
                    "Host: " + host + "\r\n" +
                    "Connection: close\r\n" +
+                   "Content-Length: " + (tempData.length()+humiData.length()) + "\r\n" +
+                   "Content-Type: text/plain\r\n" +
                    "\r\n"
                   );
-
+      client.print(tempData);
+      client.print(humiData);
+      
       /*
        * Collect the response from the host.
        */
-      PRINTln("[Response:]");
+#if SERIAL
+      Serial.println("[Response:]");
+#endif
+
+      digitalWrite(LED_PIN, LED_ON);
 
       while (client.connected())
       {
         if (client.available())
         {
           String line = client.readStringUntil('\n');
-          PRINTln(line);
+#if SERIAL
+          Serial.println(line);
+#endif
         }
       }
       client.stop();
-      PRINTln("\n[Disconnected]");
       readingSuccess = true;
+#if SERIAL
+      Serial.println("\n[Disconnected]");
+#endif
     }
     else
     {
       // The connection to the host failed.
-      PRINTln("connection failed!]");
+#if SERIAL
+      Serial.println("connection failed!]");
+#endif
       client.stop();
       readingSuccess = false;
     }
+
+    digitalWrite(LED_PIN, LED_OFF);
 
     /*
      * Delay or sleep a time appropriate depending on
